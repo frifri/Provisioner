8a74fcb4c8f5be33511d8c5fa60c3c44bc31e161
ricky ing <ricky@2600hz.com>
2014-02-12 15:29:40 -0800

    support for codec on polycom spipm

--- api/endpoint/polycom/spipm/phone.php
+++ api/endpoint/polycom/spipm/phone.php
@@ -9,11 +9,18 @@
  * @package Provisioner
  */
 class endpoint_polycom_spipm_phone extends endpoint_polycom_base {
-    public function __construct(&$config_manager) {
-        parent::__construct($config_manager);
-    }
+	public function __construct(&$config_manager) {
+		parent::__construct($config_manager);
+	}
 
-    function prepareConfig() {
-        parent::prepareConfig();
-    }
+	function prepareConfig() {
+		parent::prepareConfig();
+	
+		$settings = $this->config_manager->get_settings();
+		$settings['codecs']['audio'][$settings['codecs']['audio']['primary_codec']] = 1;
+		$settings['codecs']['audio'][$settings['codecs']['audio']['secondary_codec']] = 2; 
+		$settings['codecs']['audio'][$settings['codecs']['audio']['tertiary_codec']] = 3; 
+		$settings['codecs']['audio'][$settings['codecs']['audio']['quaternary_codec']] = 4; 
+		$this->config_manager->set_settings($settings);
+	}
 }

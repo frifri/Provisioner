bcd049d2f4fbe66f8f94b744b351cb9651781d34
frifri <francis.genet@gmail.com>
2014-02-12 15:40:34 -0800

    spaces to tabs

--- api/endpoint/base.php
+++ api/endpoint/base.php
@@ -10,23 +10,23 @@
  */
 
 abstract class endpoint_base {
-    protected $config_manager;
+	protected $config_manager;
 
-    public function __construct(&$config_manager) {
-        $this->config_manager = $config_manager;
-    }
+	public function __construct(&$config_manager) {
+		$this->config_manager = $config_manager;
+	}
 
-    public function prepareConfig() {
-        $settings = $this->config_manager->get_settings();
-        $app_settings = helper_settings::get_instance();
+	public function prepareConfig() {
+		$settings = $this->config_manager->get_settings();
+		$app_settings = helper_settings::get_instance();
 
-        if ($this->config_manager->get_request_type() == 'http') {
-            if (!empty($app_settings->custom_http_port))
-                $settings['provisioning_url'] = 'http://' . $this->config_manager->get_domain() . ':' .  $app_settings->custom_http_port . $app_settings->paths->root;
-            else
-                $settings['provisioning_url'] = 'http://' . $this->config_manager->get_domain() . $app_settings->paths->root;
-        }
-        
-        $this->config_manager->set_settings($settings);
-    }
+		if ($this->config_manager->get_request_type() == 'http') {
+			if (!empty($app_settings->custom_http_port))
+				$settings['provisioning_url'] = 'http://' . $this->config_manager->get_domain() . ':' .  $app_settings->custom_http_port . $app_settings->paths->root;
+			else
+				$settings['provisioning_url'] = 'http://' . $this->config_manager->get_domain() . $app_settings->paths->root;
+		}
+		
+		$this->config_manager->set_settings($settings);
+	}
 }
--- api/endpoint/polycom/base.php
+++ api/endpoint/polycom/base.php
@@ -9,66 +9,66 @@
  * @package Provisioner
  */
 abstract class endpoint_polycom_base extends endpoint_base {
-    public function __construct(&$config_manager) {
-        parent::__construct($config_manager);
-    }
+	public function __construct(&$config_manager) {
+		parent::__construct($config_manager);
+	}
 
-    function prepareConfig() {
-        parent::prepareConfig();
+	function prepareConfig() {
+		parent::prepareConfig();
 
-        $app_settings = helper_settings::get_instance();
+		$app_settings = helper_settings::get_instance();
 
-        $this->_set_timezone();
+		$this->_set_timezone();
 
-        $settings = $this->config_manager->get_settings();
-        if (isset($settings['hotline']) && !empty($settings['hotline'])) {
-            $number = $settings['hotline'];
+		$settings = $this->config_manager->get_settings();
+		if (isset($settings['hotline']) && !empty($settings['hotline'])) {
+			$number = $settings['hotline'];
 
-            unset($settings['hotline']);
+			unset($settings['hotline']);
 
-            $settings['hotline']['number'] = $number;
-            $settings['hotline']['enable'] = true;
-        }
+			$settings['hotline']['number'] = $number;
+			$settings['hotline']['enable'] = true;
+		}
 
-        if ($this->config_manager->get_request_type() == 'http')
-            $settings['directory_url'] = $settings['provisioning_url'] . 'directory/' . $this->config_manager->get_mac_address() . '/000000000000-directory.xml';
+		if ($this->config_manager->get_request_type() == 'http')
+			$settings['directory_url'] = $settings['provisioning_url'] . 'directory/' . $this->config_manager->get_mac_address() . '/000000000000-directory.xml';
 
-        $this->config_manager->set_settings($settings);
-    }
+		$this->config_manager->set_settings($settings);
+	}
 
-    private function _set_timezone() {
-        $constants = $this->config_manager->get_constants();
-        $settings = $this->config_manager->get_settings();
+	private function _set_timezone() {
+		$constants = $this->config_manager->get_constants();
+		$settings = $this->config_manager->get_settings();
 
-        $tz = $constants['timezone_lookup'][$settings['timezone']];
-        $strip_tz = explode(":", $tz);
+		$tz = $constants['timezone_lookup'][$settings['timezone']];
+		$strip_tz = explode(":", $tz);
 
-        $main_tz = $strip_tz[0] * 60 * 60;
-        $sub_tz = 0;
-        if (isset($strip_tz[1]))
-            $sub_tz = 30 * 60;
+		$main_tz = $strip_tz[0] * 60 * 60;
+		$sub_tz = 0;
+		if (isset($strip_tz[1]))
+			$sub_tz = 30 * 60;
 
-        $settings['timezone'] = $main_tz + $sub_tz;
+		$settings['timezone'] = $main_tz + $sub_tz;
 
-        $this->config_manager->set_settings($settings);
-    }
+		$this->config_manager->set_settings($settings);
+	}
 
-    public function setFilename($strFilename) {
-        $settings = $this->config_manager->get_settings();
+	public function setFilename($strFilename) {
+		$settings = $this->config_manager->get_settings();
 
-        if ($strFilename != '000000000000-directory.xml') {
-            //Polycoms seems to likes lower case letters in its mac address too
-            $strFilename = preg_replace('/\$mac/', strtolower($this->config_manager->get_mac_address()), $strFilename);
-        } else {
-            $folder = CONFIG_FILES_BASE . '/directory/' . $this->config_manager->get_mac_address();
-            if (!file_exists($folder))
-                mkdir($folder);
-            
-            $strFilename = 'directory/' . $this->config_manager->get_mac_address() . '/000000000000-directory.xml';
-        }
+		if ($strFilename != '000000000000-directory.xml') {
+			//Polycoms seems to likes lower case letters in its mac address too
+			$strFilename = preg_replace('/\$mac/', strtolower($this->config_manager->get_mac_address()), $strFilename);
+		} else {
+			$folder = CONFIG_FILES_BASE . '/directory/' . $this->config_manager->get_mac_address();
+			if (!file_exists($folder))
+				mkdir($folder);
+			
+			$strFilename = 'directory/' . $this->config_manager->get_mac_address() . '/000000000000-directory.xml';
+		}
 
-        return $strFilename;
-    }
+		return $strFilename;
+	}
 }
 
 ?>
